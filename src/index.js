65057d0919a59a9fb81082f526181e89da226a3a
First Guardian <raflisugiarto2004@gmail.com>
2023-07-22 14:20:51 +0700

    Fix can't use options

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin');
 
-module.exports = plugin(function ({
-  matchUtilities,
-  theme,
-  useShortClass = false,
-}) {
-  matchUtilities(
-    {
-      [useShortClass ? "g" : "gutters"]: (value) => ({
-        ["> *"]: {
-          padding: value,
-        },
-      }),
-      [useShortClass ? "gx" : "gutters-x"]: (value) => ({
-        ["> *"]: {
-          paddingLeft: value,
-          paddingRight: value,
-        },
-      }),
-      [useShortClass ? "gy" : "gutters-y"]: (value) => ({
-        ["> *"]: {
-          paddingTop: value,
-          paddingBottom: value,
-        },
-      }),
-    },
-    {
-      values: theme("spacing"),
-      variants: ["responsive"],
-    }
-  );
+module.exports = plugin.withOptions(function (options = {}) {
+  const useShortClass = Boolean(options.useShortClass);
+
+  return function ({ matchUtilities, theme }) {
+    matchUtilities(
+      {
+        [useShortClass ? 'g' : 'gutters']: (value) => ({
+          ['> *']: {
+            padding: value,
+          },
+        }),
+        [useShortClass ? 'gx' : 'gutters-x']: (value) => ({
+          ['> *']: {
+            paddingLeft: value,
+            paddingRight: value,
+          },
+        }),
+        [useShortClass ? 'gy' : 'gutters-y']: (value) => ({
+          ['> *']: {
+            paddingTop: value,
+            paddingBottom: value,
+          },
+        }),
+      },
+      {
+        values: theme('spacing'),
+        variants: ['responsive'],
+      }
+    );
+  };
 });
